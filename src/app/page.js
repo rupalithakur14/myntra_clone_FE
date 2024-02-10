ad50c734ebf8884dddccaaa4c30fc1acdf917bd1
Rupali <rupali.shethink@gmail.com>
2024-02-10 13:49:20 +0530

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,14 @@
+"use client";
+import React from "react";
+import Header from "../../components/Home/Header";
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
+
+export default Home;
+("");
